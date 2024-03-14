f1083ce822501a2224b0dabb7558387c30a392a0
Jatin <jatinkumarmid@gmail.com>
2024-03-14 16:16:18 +0530

    ios update

--- index.js
+++ index.js
@@ -20,6 +20,10 @@ app.get("/.well-known/assetlinks.json",(req,res) =>{
     res.sendFile(__dirname + "/assetlinks.json");
 })
 
+app.get("/.well-known/apple-app-site-association",(req,res) =>{
+    res.setHeader("Content-Type","application/json").sendFile(__dirname + "/apple-app-site-association");
+});
+
 app.listen(process.env.PORT || 8000,(err)=>{
     if(err){
         console.log("Server Cannot Run : ",err);
